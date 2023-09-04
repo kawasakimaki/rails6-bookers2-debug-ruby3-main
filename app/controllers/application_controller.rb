29379f03558045a98ac495959fa4c8d84f2a2080
kawasakimaki <kchurip.08@gmail.com>
2023-09-04 12:43:43 +0000

    [Fix]投稿詳細画面の編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,18 +16,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 
-  def set_request_from
-    if session[:request_from]
-      @request_from = session[:request_from]
-    end
-    session[:request_from] = request.original_url
-  end
 
-  def return_back
-    if request.referer
-      redirect_to :back and return true
-    elsif @request_from
-      redirect_to @request_from and return true
-    end
-  end
 end
